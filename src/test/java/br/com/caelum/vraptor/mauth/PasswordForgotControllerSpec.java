c5c424f613a76af4af85290a2a90dcc4984f4e96
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2012-11-26 18:53:34 -0200

    Updating simplemail and fixing tests

--- src/test/java/br/com/caelum/vraptor/mauth/PasswordForgotControllerSpec.java
+++ src/test/java/br/com/caelum/vraptor/mauth/PasswordForgotControllerSpec.java
@@ -4,21 +4,30 @@ import static br.com.caelum.vraptor.Option.none;
 import static br.com.caelum.vraptor.Option.some;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.internet.InternetAddress;
+
+import org.apache.commons.mail.Email;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.matchers.TypeSafeMatcher;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import br.com.caelum.vraptor.Option;
-import br.com.caelum.vraptor.simplemail.template.MockTemplateMailer;
+import br.com.caelum.vraptor.simplemail.Mailer;
+import br.com.caelum.vraptor.simplemail.template.TemplateMailer;
+import br.com.caelum.vraptor.simplemail.testing.MockTemplateMailer;
 import br.com.caelum.vraptor.util.test.MockResult;
-import br.com.caelum.vraptor.validator.ValidationException;
 import br.com.caelum.vraptor.view.HttpResult;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,21 +39,22 @@ public class PasswordForgotControllerSpec {
 	private PasswordForgotController controller;
 	@Mock
 	private HttpResult http;
-	private MockTemplateMailer mailer;
-	@SuppressWarnings("rawtypes")
+	private TemplateMailer templates;
 	@Mock
-	private Authenticator auth;
+	private Mailer mailer;
+	@Mock
+	private Authenticator<SystemUser> auth;
 
 	@Before
 	public void before() {
 		this.result = spy(new MockResult());
-		this.mailer = new MockTemplateMailer();
-		this.controller = new PasswordForgotController(result, users, mailer,
-				this.auth);
+		this.templates = new MockTemplateMailer();
+		this.controller = new PasswordForgotController(result, users, templates,
+				auth, mailer);
 	}
 
 	@Test
-	public void complain_if_user_email_is_not_found() {
+	public void complain_if_user_email_is_not_found() throws Exception {
 		when(users.findByEmail(null)).thenReturn(noUser());
 
 		SystemUser guilherme = new MockUser();
@@ -56,23 +66,21 @@ public class PasswordForgotControllerSpec {
 	}
 
 	@Test
-	public void send_email_to_user_when_valid_email_is_passed() {
+	public void send_email_to_user_when_valid_email_is_passed() throws Exception {
 		SystemUser user = new MockUser();
 
 		when(users.findByEmail(user.getEmail())).thenReturn(some(user));
 
 		controller.forgotPassword(user.getEmail());
-		assertTrue(mailer.sentEmailTo(user.getName(), user.getEmail()));
+
+		verify(mailer).send(argThat(emailTo(user.getName(), user.getEmail())));
 	}
 
-	@Test(expected = ValidationException.class)
+	@Test
 	public void redirect_to_login_page_if_invalid_newPasswordToken_is_passed() {
 		when(users.findForEncryptedURL(null)).thenReturn(noUser());
 		controller.resetPassword(null);
-	}
-
-	private Option<SystemUser> noUser() {
-		return none();
+		verify(result).redirectTo("/");
 	}
 
 	@Test
@@ -91,5 +99,27 @@ public class PasswordForgotControllerSpec {
 				.getPassword());
 		assertNotSame(newPasswordToken, user.getPassword()
 				.getLastEncryptedRecoveryURL());
+	}
+
+	private Option<SystemUser> noUser() {
+		return none();
+	}
+
+	private Matcher<? extends Email> emailTo(final String name, final String email) {
+		return new TypeSafeMatcher<Email>() {
+			@Override
+			public void describeTo(Description desc) {
+				desc.appendText("Email to " + name + " <" + email + ">");
+			}
+
+			@Override
+			public boolean matchesSafely(Email sent) {
+				try {
+					return sent.getToAddresses().contains(new InternetAddress(email, name));
+				} catch (UnsupportedEncodingException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		};
 	}
 }
