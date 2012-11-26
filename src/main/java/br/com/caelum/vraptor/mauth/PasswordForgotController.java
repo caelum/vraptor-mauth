280e1817a22759f4ec3f16cbb3adf2dc2afeb37b
guilherme <guilherme.silveira@caelum.com.br>
2012-11-26 12:35:10 -0200

    usando ultima versao do simplemailer

--- src/main/java/br/com/caelum/vraptor/mauth/PasswordForgotController.java
+++ src/main/java/br/com/caelum/vraptor/mauth/PasswordForgotController.java
@@ -1,10 +1,13 @@
 package br.com.caelum.vraptor.mauth;
 
+import org.apache.commons.mail.EmailException;
+
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Option;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.simplemail.Mailer;
 import br.com.caelum.vraptor.simplemail.template.TemplateMailer;
 
 @Open
@@ -15,17 +18,19 @@ public class PasswordForgotController {
 	private final Result result;
 	private final TemplateMailer mailer;
 	private final Authenticator auth;
+	private final Mailer realMailer;
 
 	PasswordForgotController(Result result, AuthUserRepository users,
-			TemplateMailer mailer, Authenticator auth) {
+			TemplateMailer mailer, Authenticator auth, Mailer realMailer) {
 		this.result = result;
 		this.users = users;
 		this.mailer = mailer;
 		this.auth = auth;
+		this.realMailer = realMailer;
 	}
 
 	@Post("/auth/accountRecovery")
-	public void forgotPassword(String email) {
+	public void forgotPassword(String email) throws EmailException {
 		Option<SystemUser> userLoaded = users.findByEmail(email);
 		if (userLoaded.isEmpty()) {
 			result.include("error", "vraptor.email_not_found");
@@ -35,8 +40,8 @@ public class PasswordForgotController {
 		SystemUser u = userLoaded.get();
 		String token = u.getPassword().generateEncryptedRecoveryText(
 				u.getEmail());
-		mailer.template("mail.passwordForgot").with("token", token)
-				.dispatchTo(u.getName(), u.getEmail());
+		realMailer.send(mailer.template("mail.passwordForgot")
+				.with("token", token).to(u.getName(), u.getEmail()));
 
 		result.include("error", "vraptor.forgot.password.email.sent");
 		result.redirectTo("/");
@@ -66,7 +71,7 @@ public class PasswordForgotController {
 		user.getPassword().generateEncryptedRecoveryText(user.getEmail());
 		users.update(user);
 		auth.authenticate(user);
-		
+
 		result.include("error", "vraptor.password_changed");
 
 		result.redirectTo("/");
